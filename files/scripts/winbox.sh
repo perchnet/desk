9cd6419c16052ba74b1d8c3964d8e0cbbd531696
bri <284789+b-@users.noreply.github.com>
2025-01-30 14:07:17 -0500

    add winbox, remove micro, move keybase to optfix

---
+++ files/scripts/winbox.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# maketmp dir
+TEMP_DIR=$(mktemp -d)
+cd "$TEMP_DIR"
+
+# get the WinBox AUR package, because that's the easiest way to check the latest version...
+curl -L https://aur.archlinux.org/cgit/aur.git/snapshot/winbox.tar.gz | tar -xzv
+cd winbox
+source PKGBUILD
+
+# "source=WinBox-4.0beta17.zip::https://download.mikrotik.com/routeros/winbox/4.0beta17/WinBox_Linux.zip"
+URL="${source##*::}"
+ZIPFile="${source%%::*}"
+curl -L "${URL}" -o "${ZIPFile}"
+unzip "${ZIPFile}"
+srcdir="$(pwd)"
+pkgdir="/"
+# pkgdir="$(mktemp -d)"
+set -x
+package
+rm -fR "${TEMP_DIR}" "${srcdir}"
