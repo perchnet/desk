d3cf863cae119aa813c76a3cb3f7b3d71118c29a
bri <284789+b-@users.noreply.github.com>
2025-08-10 01:00:56 -0400

    Update one-true-optfix.sh

--- files/scripts/one-true-optfix.sh
+++ files/scripts/one-true-optfix.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Check if /var/opt exists, if not create it
-[[ ! -d "/var/opt" ]] && mkdir -p "/var/opt"
+mkdir -p "/var/opt"
 
 print "Creating symlinks to fix packages that installed to /opt:"
 for optdir in /opt/*; do
     opt=$(basename "$optdir")
-    lib_dir="$LIB_OPT_DIR/$opt"
-    var_opt_dir="$VAR_OPT_DIR/$opt"
-    mkdir -p "$lib_dir"
-    ln -sf "$lib_dir" "$var_opt_dir"
-    echo "Created symlinks for $opt"
+    lib_opt_dir="/usr/lib/opt/$opt"
+    mv "$optdir" "$lib_opt_dir"
+    mkdir -p "$lib_opt_dir"
     tee /usr/lib/tmpfiles.d/$opt.conf <<EOF
-# create a link from $optdir to $lib_dir
-L  $optdir  -  -  -  -  $lib_dir
+# create a link from $optdir to $lib_opt_dir
+L  $optdir  -  -  -  -  $lib_opt_dir
 EOF
 done
