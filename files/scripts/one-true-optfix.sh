f34aded48e4dd2e4e2da5c0302c26efe5cfd0baf
bri <284789+b-@users.noreply.github.com>
2025-08-10 02:58:50 -0400

    Update one-true-optfix.sh

--- files/scripts/one-true-optfix.sh
+++ files/scripts/one-true-optfix.sh
@@ -3,15 +3,13 @@ set -euxo pipefail
 
 optdirs=(/opt/*)
 if [[ -n "${optdirs[*]}" ]]; then
-
-  echo "Creating symlinks to fix packages that installed to /opt:"
-  for optdir in "${optdirs[@]}"; do
-    opt=$(basename "${optdir}")
-    lib_opt_dir="/usr/lib/bluebuild-optfix/${opt}"
-    mv -v "${optdir}" "${lib_opt_dir}"
-    mkdir -pv "${lib_opt_dir}"
-    echo "linking ${optdir} => ${lib_opt_dir}"
-    echo "L  ${optdir}  -  -  -  -  ${lib_opt_dir}" > "/usr/lib/tmpfiles.d/${opt}-bluebuild.conf"
-EOF
-  done
+    echo "Creating symlinks to fix packages that installed to /opt:"
+    for optdir in "${optdirs[@]}"; do
+        opt=$(basename "${optdir}")
+        lib_opt_dir="/usr/lib/bluebuild-optfix/${opt}"
+        mv -v "${optdir}" "${lib_opt_dir}"
+        mkdir -pv "${lib_opt_dir}"
+        echo "linking ${optdir} => ${lib_opt_dir}"
+        echo "L  ${optdir}  -  -  -  -  ${lib_opt_dir}" > "/usr/lib/tmpfiles.d/${opt}-bluebuild.conf"
+    done
 fi
